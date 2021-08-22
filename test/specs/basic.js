d21d3dc33b2d3266acd5228569d7f57810bd58bb
hamidmunir <hmunir@citymd.net>
2021-08-22 19:54:02 +0500

    Initial commit

---
+++ test/specs/basic.js
@@ -0,0 +1,23 @@
+describe('webdriver.io page', () => {
+
+    it('should have the right title', async() => {
+        browser.url('https://www.ebay.com/')
+        browser.maximizeWindow()
+        const title = browser.getTitle()
+        expect(browser).toHaveTitle('Electronics, Cars, Fashion, Collectibles & More | eBay')
+        
+    });
+
+    it('should search a product on amazon', async() => {
+      
+      const search= await $('#gh-ac') ;//search field
+      
+      const searchButton= await $('#gh-btn');//search button
+      await search.click();
+      //search.clearValue();
+      await search.addValue('Mobile phone');
+      await searchButton.click();
+      await expect(search).toHaveValue('mobile phone');
+
+    });
+});
